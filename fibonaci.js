0f75ef7bfd3ef54927ac5c2d0685c2fb93ab7425
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-04-27 18:53:11 -0400

    series fibonacci

--- fibonaci.js
+++ fibonaci.js
@@ -1,1 +1,23 @@
-console.log("fibonaci")
+
+function SeriesFibonacci(n) {
+    if (n == 0){
+        return 0}
+    if (n == 1){
+        return 1}
+    return SeriesFibonacci(n-1) + SeriesFibonacci(n-2)}
+
+const terminal = require('readline');
+
+const Console = terminal.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function numero(num){
+    return num
+}
+Console.question("\nEnter a number ", (number) => {
+
+    console.log("F( "+numero(number)+" ) = " + SeriesFibonacci(number))
+    Console.close();
+    });
