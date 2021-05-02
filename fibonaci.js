4dc5a1192d467b361f60eaee92f5ecca8af5a0cf
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-05-01 22:21:57 -0400

    una pequeñisima modificacion

--- fibonaci.js
+++ fibonaci.js
@@ -1,5 +1,6 @@
 
 function SeriesFibonacci(n) {
+    
     if (n == 0){return 0}
     if (n == 1){return 1}
     return SeriesFibonacci(n-1) + SeriesFibonacci(n-2)}
@@ -12,10 +13,17 @@ const Console = terminal.createInterface({
 });
 
 function numero(num){
-    return num
+    if(num >= 0){
+        return num
+    }
+    else{
+        //console.log("el numero no puede ser una letra o negativo se cambiara a 0")
+        return 0
+    }
+
 }
 Console.question("\nEnter a number ", (number) => {
 
-    console.log("F( "+numero(number)+" ) = " + SeriesFibonacci(number))
+    console.log("F( "+numero(number)+" ) = " + SeriesFibonacci(numero(number)))
     Console.close();
     });
